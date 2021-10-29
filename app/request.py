37cb533baf53dfb9f3bef2cc7454327af1e7d8fa
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-29 09:53:58 +0300

    deploy to git

--- app/request.py
+++ app/request.py
@@ -2,6 +2,7 @@ from app import app
 import urllib.request,json
 from .models import movie
 
+
 Movie = movie.Movie
 
 # Getting api key
@@ -21,7 +22,7 @@ def get_movies(category):
         get_movies_data = url.read()
         get_movies_response = json.loads(get_movies_data)
 
-        movie_results = None
+        movie_results = None  #replacable with []
 
         if get_movies_response['results']:
             movie_results_list = get_movies_response['results']
@@ -53,4 +54,24 @@ def process_results(movie_list):
             movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
             movie_results.append(movie_object)
 
-    return movie_results    
+    return movie_results
+
+def get_movie(id):
+    get_movie_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_movie_details_url) as url:
+        movie_details_data = url.read()
+        movie_details_response = json.loads(movie_details_data)
+
+        movie_object = None  #replacable with []
+        if movie_details_response:
+            id = movie_details_response.get('id')
+            title = movie_details_response.get('original_title')
+            overview = movie_details_response.get('overview')
+            poster = movie_details_response.get('poster_path')
+            vote_average = movie_details_response.get('vote_average')
+            vote_count = movie_details_response.get('vote_count')
+
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+
+    return movie_object    
